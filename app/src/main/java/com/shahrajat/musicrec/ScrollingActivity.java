4093af742c530eb7cb354f1354ed3a1b1438c975
Rajat Shah <rajat.shah200792@gmail.com>
2016-11-01 16:01:21 -0400

    adding songs.xml

--- app/src/main/java/com/shahrajat/musicrec/ScrollingActivity.java
+++ app/src/main/java/com/shahrajat/musicrec/ScrollingActivity.java
@@ -22,7 +22,7 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Playing Song now...", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
