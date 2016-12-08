c92586b1d2b4665095e05fca306dfcefc8ae21cd
Rajat Shah <rajat.shah200792@gmail.com>
2016-12-07 23:17:26 -0500

    bug fixes for realtime middleware access

--- musicrecmiddleware/src/main/java/com/shahrajat/musicrecmiddleware/ContextMiddlewareService.java
+++ musicrecmiddleware/src/main/java/com/shahrajat/musicrecmiddleware/ContextMiddlewareService.java
@@ -1,17 +1,30 @@
 package com.shahrajat.musicrecmiddleware;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.location.LocationManager;
 import android.media.AudioManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
-public class ContextMiddlewareService extends Service {
+public class ContextMiddlewareService extends Service implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mGoogleApiClient;
+    final DetectedActivity userActivity = null;
 
     public ContextMiddlewareService() {
     }
@@ -19,7 +32,33 @@ public class ContextMiddlewareService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        // For activity recognition
+        mGoogleApiClient = new GoogleApiClient.Builder(this).build();
 
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    // Activity recognition service
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        try {
+            Intent intent = new Intent( this, ContextMiddlewareService.class );
+            PendingIntent pendingIntent = PendingIntent.getService( this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+            ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates( mGoogleApiClient, 3000, pendingIntent );
+
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
@@ -31,13 +70,13 @@ public class ContextMiddlewareService extends Service {
     private final IContextInterface.Stub mBinder = new IContextInterface.Stub() {
 
         /*
-         * This function immediately returns the result without using a callback interface.
-         * Avoid using functions that work synchronously if there is a possibility of delay in getting the required response.
+        Returns the string for current activity of the user
          */
+
 
         public String getUserActivity() {
             String activity = "";
-            final DetectedActivity userActivity = null;
+
             switch (userActivity.getType()) {
                 case DetectedActivity.IN_VEHICLE:
                     activity = "driving";
